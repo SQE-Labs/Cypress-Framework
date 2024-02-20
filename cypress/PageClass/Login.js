7a26c39adfb587d817de36c8320face7895395f3
saksham-sqelabs <saksham.sharma@sqelabs.com>
2024-02-20 18:18:17 +0530

    changes for admin creation through SuperAdmin

--- cypress/PageClass/Login.js
+++ cypress/PageClass/Login.js
@@ -1,15 +1,17 @@
-const locator_xpath = require("../locators/xpath/xpath.json");
+const locator_xpath = require("../locators/xpath/superadmin.json");
+import { generateRandomString, generateRandomNumber } from "../support/utils";
 
 class Login {
-  clickLogin() {
-    cy.xpath(locator_xpath.loginbtn).click();
-    cy.xpath("//h5[.='Log in']").should("be.visible");
-  }
+  // clickLogin() {
+  //   cy.xpath(locator_xpath.loginbtn).click();
+  //   cy.xpath("//h5[.='Log in']").should("be.visible");
+  // }
 
   setUserName(username) {
     cy.wait(2000);
     cy.xpath(locator_xpath.usernametxt).click().type(username);
   }
+
   setPassword(password) {
     cy.xpath(locator_xpath.passwordtxt).click().type(password);
   }
@@ -17,8 +19,83 @@ class Login {
   clickLoginbtn() {
     cy.xpath(locator_xpath.signInbtn).click();
   }
+
   verifyLoginSuccessMsg(successMsg) {
     cy.xpath(locator_xpath.welcomemsg).should("have.text", successMsg);
+  }
+
+  verifyUserOnDashboard() {
+    cy.xpath(locator_xpath.dashboard).should("have.text", "Dashboard");
+  }
+
+  clickAdminCreationTab() {
+    cy.xpath(locator_xpath.adminTab).click();
+  }
+
+  enterAdminFirstName(adminFirstName) {
+    cy.xpath(locator_xpath.adminFirstName)
+      .click()
+      .type(adminFirstName + generateRandomString(3));
+  }
+
+  enterAdminLastName(adminLastName) {
+    cy.xpath(locator_xpath.adminLastName)
+      .click()
+      .type(adminLastName + generateRandomString(3));
+  }
+
+  enterAdminMobileNumber(adminMobileNumber) {
+    cy.xpath(locator_xpath.adminMobileNumber).click().type(adminMobileNumber);
+  }
+
+  enterAdminEmail(adminEmail) {
+    cy.xpath(locator_xpath.adminEmail)
+      .click()
+      .type(adminEmail + generateRandomString(3) + "@yopmail.com");
+  }
+
+  enterUserName(userName) {
+    cy.xpath(locator_xpath.adminUsername).click().type(userName);
+  }
+
+  enterNewPassword(passwordField) {
+    cy.xpath(locator_xpath.adminCreatepassword).click().type(passwordField);
+  }
+
+  confirmPasswordField(confirmPasswordText) {
+    cy.xpath(locator_xpath.adminConfirmpassword)
+      .click()
+      .type(confirmPasswordText);
+  }
+
+  clickOnCreateAdmin() {
+    cy.xpath(locator_xpath.adminCreationSubmission).click();
+  }
+
+  clickOnOpeningAdminCreation() {
+    cy.xpath(locator_xpath.adminCreationForm).click();
+  }
+
+  createAdmin(
+    adminFirstName,
+    adminLastName,
+    adminMobileNumber,
+    adminEmail,
+    userName,
+    password,
+    confirmPassword
+  ) {
+    this.enterAdminFirstName(adminFirstName);
+    this.enterAdminLastName(adminLastName);
+    this.enterAdminMobileNumber(adminMobileNumber);
+    this.enterAdminEmail(adminEmail);
+    this.enterUserName(userName);
+    this.enterNewPassword(password);
+    this.confirmPasswordField(confirmPassword);
+  }
+
+  verifyAdminOnDashboard(adminCreated) {
+    cy.xpath(locator_xpath.adminListUsername).should("have.text", adminCreated);
   }
 }
 
